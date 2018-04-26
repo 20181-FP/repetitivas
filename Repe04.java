9098f370ee95fbcfdddf59467dbec07fe54a31e4
Angel Sullon <asullom@gmail.com>
2018-04-26 18:18:28 -0500

    bucle ok

--- Repe04.java
+++ Repe04.java
@@ -2,40 +2,36 @@ import java.util.Scanner;
 
 public class Repe04 {
   public static void main(String[] args) {
-
     /* Menu
       0: salir
       1: Sumar
       2: Restar
     */
     Scanner reader = new Scanner(System.in);
-    
-
-    System.out.print("Elija Menu:\n");
-    System.out.println("0: salir");
-    System.out.println("1: sumar");
-    System.out.println("2: restar");
-
-    String d = reader.nextLine();
     do {
-    System.out.print("Elija Menu:\n");
-    System.out.println("0: salir");
-    System.out.println("1: sumar");
-    System.out.println("2: restar");
-    d = reader.nextLine();
-    switch (d) {
-            case "0":  System.out.print("Bye");
-                      System.exit(0);
-                     break;
-            case "1":  System.out.print("Suma es:");
-                     break;
-            case "2":  System.out.print("Resta es:");
-                     break;
-            
-            default: System.out.print("Opcion no valido");
-                     break;
-        }
-    System.out.print("Selecciono="+d);
-  } while (true);
+      System.out.print("Elija Menu:\n");
+      System.out.println("0: salir");
+      System.out.println("1: sumar");
+      System.out.println("2: restar\n");
+      int d = reader.nextInt();
+      switch (d) {
+              case 0:  System.out.print("Bye");
+                        System.exit(0);
+                       break;
+              case 1:  
+                    System.out.print("Ingrese a y b:");
+                    int a = reader.nextInt();
+                    int b = reader.nextInt();
+                    System.out.println("Suma es:" +(a+b));
+                      
+                       break;
+              case 2:  System.out.println("Resta es:");
+                       break;
+              
+              default: System.out.println("Opcion no valido");
+                       break;
+      }
+      System.out.println("Selecciono="+d);
+    } while (true);
   }
-  }
+}
