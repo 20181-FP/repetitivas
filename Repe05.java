0b6b76c24809940a36c370a7582aaa7f833467bb
Angel Sullon <asullom@gmail.com>
2018-04-26 19:13:49 -0500

    suma en func

--- Repe05.java
+++ Repe05.java
@@ -1,19 +1,13 @@
 import java.util.Scanner;
 
-public class Repe04 {
+public class Repe05 {
+
   public static void main(String[] args) {
-    /* Menu
-      0: salir
-      1: Sumar
-      2: Restar
-    */
+
     Scanner reader = new Scanner(System.in);
     int d = -1;
     do {
-      System.out.print("\nElija Menu:\n");
-      System.out.println("0: salir");
-      System.out.println("1: sumar");
-      System.out.println("2: restar");
+      menu();
       d = reader.nextInt();
       switch (d) {
               case 0:  System.out.print("Bye");
@@ -23,7 +17,7 @@ public class Repe04 {
                     System.out.print("Ingrese a y b:");
                     int a = reader.nextInt();
                     int b = reader.nextInt();
-                    System.out.println("Suma es:" +(a+b));
+                    System.out.println("Suma es:" +suma(a,b));
                       
                        break;
               case 2:  
@@ -35,6 +29,20 @@ public class Repe04 {
       System.out.println("Selecciono="+d);
     } while (d!=0);
   }
-  
-  
+
+  public static void menu(){
+    /* Menu
+      0: salir
+      1: Sumar
+      2: Restar
+    */
+      System.out.print("\nElija Menu:\n");
+      System.out.println("0: salir");
+      System.out.println("1: sumar");
+      System.out.println("2: restar");
+  }
+  public static int suma(int a, int b){
+    return a+b;
+  }
+
 }
